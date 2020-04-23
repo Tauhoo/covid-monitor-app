c618bd58b2219c2e36f91594da15da56dccff616
wachirawit wacharak <tauhoo@wachirawits-MacBook-Pro.local>
2020-04-23 16:19:08 +0700

    add internet checker

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import NetInfo from "@react-native-community/netinfo"
 import {
   StyleSheet,
   ScrollView,
@@ -14,46 +15,68 @@ import { queryNotification } from "./api"
 export default function App() {
   const [notification, updateNotification] = useState([])
   const [loading, setLoading] = useState(true)
+  const [internetStatus, setInternetStatus] = useState(true)
 
   useEffect(() => {
     async function fetchNotification() {
-      const res = await queryNotification()
-      updateNotification(res.data.Data)
-      setLoading(false)
+      try {
+        const res = await queryNotification()
+        updateNotification(res.data.Data)
+        setLoading(false)
+      } catch (e) {
+        console.log(e)
+      }
     }
     fetchNotification()
   }, [loading])
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) =>
+      setInternetStatus(state.isConnected)
+    )
+    return unsubscribe
+  }, [])
+
+  const content = loading ? null : (
+    <ScrollView>
+      <View style={styles.container}>
+        {notification.map((props, index) => (
+          <Notification key={index + ""} {...props} />
+        ))}
+        <Status></Status>
+        <Stat></Stat>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.credit}>ข้อมูลจาก https:covid19.th-stat.com</Text>
+        <Text style={styles.credit}>เซิร์ฟเวอร์โดย https:smilehost.asia</Text>
+        <Text style={styles.credit}>พัตนาข้อมูลโดย www.kidkarnmai.com</Text>
+        <Text style={styles.credit}>
+          พัตนาแอพพลิเคชั่นโดย วชิรวิทย์ เวชรักษ์
+        </Text>
+      </View>
+    </ScrollView>
+  )
 
   return (
     <View style={styles.background}>
       <View style={styles.navbar}>
         <Text style={{ color: "white", fontSize: 20 }}>โควิด มอนิเตอร์</Text>
         <TouchableOpacity onPress={() => setLoading(true)}>
-          <Text style={{ color: "white", fontSize: 15 }}>รีเฟรช</Text>
+          <Text style={{ color: "white", fontSize: 15 }}>
+            {internetStatus ? "รีเฟรช" : ""}
+          </Text>
         </TouchableOpacity>
       </View>
-      {loading ? null : (
-        <ScrollView>
-          <View style={styles.container}>
-            {notification.map((props, index) => (
-              <Notification key={index + ""} {...props} />
-            ))}
-            <Status></Status>
-            <Stat></Stat>
-          </View>
-          <View style={styles.footer}>
-            <Text style={styles.credit}>
-              ข้อมูลจาก https:covid19.th-stat.com
-            </Text>
-            <Text style={styles.credit}>
-              เซิร์ฟเวอร์โดย https:smilehost.asia
-            </Text>
-            <Text style={styles.credit}>พัตนาข้อมูลโดย www.kidkarnmai.com</Text>
-            <Text style={styles.credit}>
-              พัตนาแอพพลิเคชั่นโดย วชิรวิทย์ เวชรักษ์
+      {internetStatus ? (
+        content
+      ) : (
+        <View style={styles.container}>
+          <View style={styles.warning}>
+            <Text style={{ color: "white", fontSize: 20 }}>
+              โปรดเชื่อมต่ออินเตอร์เน็ต
             </Text>
           </View>
-        </ScrollView>
+        </View>
       )}
     </View>
   )
@@ -72,6 +95,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingBottom: 10,
     paddingTop: 30,
+    marginBottom: 5,
     backgroundColor: "#6c5ce7",
     flexDirection: "row",
     alignItems: "center",
@@ -84,5 +108,13 @@ const styles = StyleSheet.create({
   credit: {
     color: "white",
     marginVertical: 5,
+  },
+  warning: {
+    backgroundColor: "#d63031",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 10,
   },
 })
